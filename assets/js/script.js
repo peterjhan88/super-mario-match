3698c78ad9e3a8fe31cc29baaa9fe638dcafb02a
Peter Han <peterjhan88@gmail.com>
2019-11-12 18:42:48 -0800

    check match with 3 cards, not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(initializeApp);
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
 
 function initializeApp(){
 
@@ -6,7 +9,33 @@ function initializeApp(){
 }
 
 function handleCardClick(event){
+
   var selectedCard = $(event.currentTarget);
   selectedCard.addClass("hidden");
-  setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
+
+  if (!firstCardClicked) {
+    firstCardClicked = $(event.currentTarget).parent().children().find(".front");
+  } else {
+    secondCardClicked = $(event.currentTarget).parent().children().find(".front");
+  }
+  if (firstCardClicked && secondCardClicked){
+    var firstCardUrl = $(firstCardClicked).css("background-image");
+    var secondCardUrl = $(secondCardClicked).css("background-image");
+    if(firstCardUrl===secondCardUrl){
+      console.log("cards match");
+      matches+=1;
+      firstCardClicked.addClass("hidden");
+      secondCardClicked.addClass("hidden");
+      firstCardClicked=null;
+      secondCardClicked=null;
+    } else {
+      setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    }
+  } else {
+    setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
+    firstCardClicked = null;
+    secondCardClicked = null;
+  }
 }
