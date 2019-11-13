4e74dfebb6ae2e8865942169b259b98762a16072
Peter Han <peterjhan88@gmail.com>
2019-11-13 13:30:31 -0800

    4 cards matching, modal added, some stats[F

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,66 @@
 $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
+
 var matches = null;
+var attempts = null;
+var max_matches = 2;
+
+var pictures = ["css-logo.png",'docker-logo.jpg','gitHub-logo.png',
+                'html-logo.png','js-logo.png','mysql-logo.png',
+                'node-logo.png','php-logo.jpeg','react-logo.png'];
+
 
 function initializeApp(){
+  $('.card').on("click", handleCardClick);
 
-  $('.card > .back').on("click", handleCardClick);
+
 }
 
 function handleCardClick(event){
-
   var selectedCard = $(event.currentTarget);
-  selectedCard.addClass("hidden");
+  selectedCard.find('.back').addClass("hidden");
 
   if (!firstCardClicked) {
-    firstCardClicked = $(event.currentTarget).parent().children().find(".front");
+    firstCardClicked = selectedCard.addClass('clicked');
   } else {
-    secondCardClicked = $(event.currentTarget).parent().children().find(".front");
+    secondCardClicked = selectedCard.addClass('clicked');
   }
-  if (firstCardClicked && secondCardClicked){
-    var firstCardUrl = $(firstCardClicked).css("background-image");
-    var secondCardUrl = $(secondCardClicked).css("background-image");
-    if(firstCardUrl===secondCardUrl){
-      console.log("cards match");
-      matches+=1;
-      firstCardClicked.addClass("hidden");
-      secondCardClicked.addClass("hidden");
-      firstCardClicked=null;
-      secondCardClicked=null;
+  // debugger;
+  if(firstCardClicked  &&  secondCardClicked){
+    var firstCardUrl = firstCardClicked.find(".front").css("background-image");
+    var secondCardUrl = secondCardClicked.find(".front").css("background-image");
+
+    if (firstCardUrl !== secondCardUrl){ // no match
+      console.log("NOPE!")
+      setTimeout(function () {
+        firstCardClicked.find('.back').removeClass("hidden");
+        secondCardClicked.find('.back').removeClass("hidden");
+        firstCardClicked.removeClass("chlicked");
+        secondCardClicked.removeClass("clicked");
+        firstCardClicked = null;
+        secondCardClicked = null;
+        // for later use, back to null
+      }, 1000);
     } else {
-      setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
+      console.log("WOW!!!!!!");
+      matches += 1;
+      firstCardClicked.filter(".back").addClass("hidden");
+      secondCardClicked.filter(".back").addClass("hidden");
       firstCardClicked = null;
       secondCardClicked = null;
     }
-  } else {
-    setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
-    firstCardClicked = null;
-    secondCardClicked = null;
+    attempts +=1;
+    updateAttempts();
+    updateMatch();
   }
+}
+
+function updateMatch(){
+  var accuracy = Math.round(matches * 10000 / attempts) /100;
+  $("#accuracy").text(accuracy+"%");
+}
+
+function updateAttempts(){
+  $("#attempts").text(attempts);
 }
