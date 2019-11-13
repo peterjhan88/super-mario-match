f58e36e7344f4d34deca5809fbb0ea7d9130faf7
Peter Han <peterjhan88@gmail.com>
2019-11-12 17:52:35 -0800

    single card - flipping like feature added on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+
+  $('.card > .back').on("click", handleCardClick);
+}
+
+function handleCardClick(event){
+  var selectedCard = $(event.currentTarget);
+  selectedCard.addClass("hidden");
+  setTimeout(function(){selectedCard.removeClass("hidden");}, 3000);
+}
