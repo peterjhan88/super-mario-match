63f72abf26a64e862f1fc4aaa9ae1ac79cadcc1a
Peter Han <peterjhan88@gmail.com>
2019-11-13 16:23:50 -0800

    functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,32 @@ var firstCardClicked = null;
 var secondCardClicked = null;
 
 var matches = null;
-var attempts = null;
-var max_matches = 2;
+var attempts = 0;
+var max_matches = 1;
+var gameNumber = 1;
 
 var pictures = ["css-logo.png",'docker-logo.jpg','gitHub-logo.png',
-                'html-logo.png','js-logo.png','mysql-logo.png',
+                'html-logo.png','js-logo.png','mysql-logo.jpg',
                 'node-logo.png','php-logo.jpeg','react-logo.png'];
 
 
 function initializeApp(){
-  $('.card').on("click", handleCardClick);
 
+  newGame();
+
+  $('.card').on("click", handleCardClick);
+  $("#myModalBtnYes").on("click", newGame);
+  $("#myModalBtnNo").on("click", closeModal);
 
 }
 
 function handleCardClick(event){
   var selectedCard = $(event.currentTarget);
   selectedCard.find('.back').addClass("hidden");
-
   if (!firstCardClicked) {
     firstCardClicked = selectedCard.addClass('clicked');
-  } else {
+    return;
+  } else if (!selectedCard.hasClass('clicked') && !secondCardClicked){
     secondCardClicked = selectedCard.addClass('clicked');
   }
   // debugger;
@@ -36,12 +41,12 @@ function handleCardClick(event){
       setTimeout(function () {
         firstCardClicked.find('.back').removeClass("hidden");
         secondCardClicked.find('.back').removeClass("hidden");
-        firstCardClicked.removeClass("chlicked");
+        firstCardClicked.removeClass("clicked");
         secondCardClicked.removeClass("clicked");
         firstCardClicked = null;
         secondCardClicked = null;
         // for later use, back to null
-      }, 1000);
+      }, 500);
     } else {
       console.log("WOW!!!!!!");
       matches += 1;
@@ -54,13 +59,70 @@ function handleCardClick(event){
     updateAttempts();
     updateMatch();
   }
+
+  if(matches === max_matches){
+    $('.myModal').removeClass('hidden');
+  }
 }
 
 function updateMatch(){
-  var accuracy = Math.round(matches * 10000 / attempts) /100;
-  $("#accuracy").text(accuracy+"%");
+  if(attempts!==0){
+    var accuracy = Math.round(matches * 10000 / attempts) /100;
+    $("#accuracy").text(accuracy+"%");
+  } else {
+    $("#accuracy").text(0+"%");
+  }
 }
 
 function updateAttempts(){
   $("#attempts").text(attempts);
+}
+
+function closeModal(){
+  $('.myModal').addClass("hidden");
+}
+
+function newGame(){
+  matches =0;
+  attempts=0;
+  updateAttempts();
+  updateMatch();
+  updateGamePlayed();
+  var randomArrayOfPictures = [];
+  randomArrayOfPictures = pictures.concat(pictures);
+  randomArrayOfPictures = randomOrder(randomArrayOfPictures);
+  closeModal();
+  populateCards(randomArrayOfPictures);
+  $('.card > .back').removeClass('hidden');
+  $('.card > .back').removeClass('clicked');
+}
+
+function randomOrder(picturesArr) {
+  var copyOfPicturesArr = JSON.parse(JSON.stringify(picturesArr));
+  var newRandomizedArr = [];
+  while (copyOfPicturesArr.length !== 0) {
+    var randomIndex = Math.floor(Math.random() * copyOfPicturesArr.length);
+    newRandomizedArr.push(copyOfPicturesArr[randomIndex]);
+    copyOfPicturesArr.splice(randomIndex, 1);
+  }
+  return newRandomizedArr;
+}
+
+function populateCards(picturesArray){
+  var all_cards = $('.card');
+  var frontCardDivs = all_cards.find(".front");
+  var pathFront = "url('./assets/images/";
+  var completePath = "";
+
+  for (var index = 0; index < frontCardDivs.length; index++){
+    completePath = pathFront + picturesArray[index] +"')"
+    $(frontCardDivs[index]).css({
+      "background-image": completePath
+    })
+  }
+}
+
+function updateGamePlayed(){
+  $('#gameRounds').text(gameNumber);
+  gameNumber++;
 }
